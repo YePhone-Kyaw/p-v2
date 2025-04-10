42b62664c1624b3d23ee44008403fd8ce9a099a5
Ye Phone Kyaw <yephonekyaw920@gmail.com>
2025-04-10 00:10:46 -0600

    update commit

--- src/components/awards/YycHack.jsx
+++ src/components/awards/YycHack.jsx
@@ -4,7 +4,7 @@ const YycHack = () => {
   return (
     <div>
         <a href="https://livewirecalgary.com/2024/02/05/ yyc-hacks-2024-hackathon-winner-gives-newcomers-info-right-after-they-land-in-calgary/">
-        <img src="/yyc-hack.png" alt="yyc-hack" className='h-60'/>
+        <img src="/yyc-hack.png" alt="yyc-hack" className='image h-60'/>
         </a>
     </div>
   )
