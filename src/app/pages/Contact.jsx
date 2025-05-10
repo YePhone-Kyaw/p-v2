09557f81fa2900d71bb2b7a42c9a65aa1d4d9cec
Ye Phone Kyaw <yephonekyaw920@gmail.com>
2025-05-09 23:57:31 -0600

    update

--- src/app/pages/Contact.jsx
+++ src/app/pages/Contact.jsx
@@ -34,7 +34,21 @@ const Contact = () => {
     <section id='contact'>
       <div className='flex flex-col items-center justify-center '>
       <h3 className='flex flex-col items-center justify-center font-bold text-5xl text-teal-300'>Get in touch!</h3>
-      <p className='mt-5 text-lg text-gray-500'>Whether you have a question, want to collaborate, want to hire, or just want to say hello, feel free to drop a message!</p>
+      <p className='mt-5 text-lg' style={{color: '#7593af'}}>Whether you have a question, want to collaborate, want to hire, or just want to say hello, feel free to drop a message!</p>
+      <form>
+        <div>
+          <label>Name</label>
+          <input
+           type='text'
+           id='name'
+           name='name'
+           value={name}
+           onChange={handleSetName}
+           className="w-full  border border-gray-300 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-teal-400 transition cursor-pointer"
+           placeholder='Your full name'
+           />
+        </div>
+      </form>
       </div>
    
       </section>
