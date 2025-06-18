c4b4e1203561cd29bf90bc347a4e3a7588d06187
Ye Phone Kyaw <yephonekyaw920@gmail.com>
2025-06-17 23:30:54 -0600

    update

--- src/components/TextCard.jsx
+++ src/components/TextCard.jsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const TextCard = ({ children, icon, title }) => {
   return (
-    <div className="flex gap-3 p-3 bg-teal-300 opacity-80 rounded-lg w-full">
-      {icon}
+    <div className="flex gap-3 p-3 border-2 border-teal-300 opacity-80 rounded-lg w-full">
+      <div>{icon}</div>
       <div>
-        <div>{title}</div>
+        <div className="text-">{title}</div>
         <div>{children}</div>
       </div>
     </div>
