aff31bbe6a45e8a97d03b806daee83d454873b58
Ye Phone Kyaw <yephonekyaw920@gmail.com>
2025-06-17 23:55:32 -0600

    update

--- src/components/icons/web.jsx
+++ src/components/icons/web.jsx
@@ -17,7 +17,6 @@ const IconWebDevelopment = () => (
     <circle cx="7" cy="8" r="1" />
     <circle cx="11" cy="8" r="1" />
     <circle cx="15" cy="8" r="1" />
-    {/* Code brackets */}
     <polyline points="8 16 6 14 8 12" />
     <polyline points="16 12 18 14 16 16" />
     <line x1="12" y1="12" x2="12" y2="16" />
